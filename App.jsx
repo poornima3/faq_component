efbfa921b188a09501173a4102f8cf17d988cbca
poornima3 <31poornima@gmail.com>
2024-09-19 18:03:32 +0530

    Adding files
    Adding files

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import FAQComp from './FAQComp';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <h1>Frequently Asked Questions</h1>
+      <FAQComp />
+    </div>
+  )
+}
+
+export default App
